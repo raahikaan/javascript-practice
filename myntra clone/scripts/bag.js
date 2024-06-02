ee4b99ad8a635f89168ab2fc525c0bf5dc4aebcb
RAahi KaaN <shoaibmkr0@gmail.com>
2024-06-03 04:02:56 +0500

    Myntra clone updated

--- myntra clone/scripts/bag.js
+++ myntra clone/scripts/bag.js
@@ -1,9 +1,53 @@
+const CONVENIENCE_FEES = 99;
 let bagItemsObj;
 onLoad();
 
 function onLoad() {
   loadBagItemsObj();
   displayBagItems();
+  displayBagSummary();
+}
+
+function displayBagSummary() {
+  let bagSummaryElement = document.querySelector(".bag-summary");
+  let totalItem = bagItemsObj.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+
+  bagItemsObj.forEach((bagItem) => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+
+  let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+
+  bagSummaryElement.innerHTML = `
+  <div class="bag-details-container">
+  <div class="price-header">PRICE DETAILS (${totalItem} Items)</div>
+  <div class="price-item">
+    <span class="price-item-tag">Total MRP</span>
+    <span class="price-item-value">₹ ${totalMRP} </span>
+  </div>
+  <div class="price-item">
+    <span class="price-item-tag">Discount on MRP</span>
+    <span class="price-item-value priceDetail-base-discount"
+      >-₹ ${totalDiscount} </span
+    >
+  </div>
+  <div class="price-item">
+    <span class="price-item-tag">Convenience Fee</span>
+    <span class="price-item-value">₹ 99</span>
+  </div>
+  <hr />
+  <div class="price-footer">
+    <span class="price-item-tag">Total Amount</span>
+    <span class="price-item-value">₹ ${finalPayment} </span>
+  </div>
+</div>
+<button class="btn-place-order">
+  <div class="css-xjhrni">PLACE ORDER</div>
+</button>
+  `;
 }
 
 function loadBagItemsObj() {
@@ -33,6 +77,7 @@ function removeFromBag(itemId) {
   loadBagItemsObj();
   displayBagItems();
   displayBagIcon();
+  displayBagSummary();
 }
 
 function generateItemHtml(item) {
